8c242ee51b58d27f801568e02da9504b5515bb28
fsoteras-hue <fsoteras@icloud.com>
2026-04-24 22:50:23 -0300

    Initial commit: TorchSharp hello world with tensor examples

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TorchSharp;
+using static TorchSharp.torch;
+
+// Create a tensor with 3 elements
+
+var t = tensor(new double[] { 1, 2, 3 });
+// Create a 2x3 matrix of zeros
+var zeros = torch.zeros(new long[] { 2, 3 });
+Console.WriteLine("Tensor t:");
+Console.WriteLine(t);
+Console.WriteLine("Zeros matrix:");
+Console.WriteLine(zeros);
